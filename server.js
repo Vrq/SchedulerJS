4a4910f5296c65d2e4378774c87b30129a61ad70
Janusz Tomasik <jtomasik@guidewire.com>
2017-01-30 14:34:12 +0100

    change port

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 app.get('/', function(req, res) {
    res.sendFile(__dirname + '/public/index.html');
  });
-app.set('port', 8080);
+port = (process.env.PORT || '3000')
+app.set('port', port);
 
 var server = http.createServer(app);
-server.listen(8080);
+server.listen(port);
