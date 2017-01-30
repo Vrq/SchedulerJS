3609e27bac87d67981ecf63027c721fbe92794c6
Janusz Tomasik <jtomasik@guidewire.com>
2017-01-30 14:35:55 +0100

    use original serverjs

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 const express = require('express');
-const http = require('http');
+const multer = require('multer');
+
 const app = express();
 
+//file upload config
+const storage = multer.diskStorage({
+    destination: './uploaded_files',
+    filename: function(req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+const uploadingConfig = multer({
+  storage: storage,
+  limits: {filesize: 10000000}
+}).array('files')
+
+app.use(express.static('public'));
+
+
+//Routes:
 app.get('/', function(req, res) {
    res.sendFile(__dirname + '/public/index.html');
  });
-port = (process.env.PORT || '3000')
-app.set('port', port);
+app.post('/upload', uploadingConfig, function(req, res) {
+  console.log('Z serwera 3');
+  console.log(req.files[0])
+  res.status(200).send('OK');
+});
+// to do: parsowanie pliku ze ściezki na serwerze
 
-var server = http.createServer(app);
-server.listen(port);
+port = (process.env.PORT || '3000')
+
+app.listen(port, function() {
+   console.log("Listening, sir.");
+});
--- server_backup.js
+++ server_backup.js
@@ -1,34 +1,12 @@
 const express = require('express');
-const multer = require('multer');
-
+const http = require('http');
 const app = express();
 
-//file upload config
-const storage = multer.diskStorage({
-    destination: './uploaded_files',
-    filename: function(req, file, cb) {
-      cb(null, file.originalname)
-    }
-})
-const uploadingConfig = multer({
-  storage: storage,
-  limits: {filesize: 10000000}
-}).array('files')
-
-app.use(express.static('public'));
-
-
-//Routes:
 app.get('/', function(req, res) {
    res.sendFile(__dirname + '/public/index.html');
  });
-app.post('/upload', uploadingConfig, function(req, res) {
-  console.log('Z serwera 3');
-  console.log(req.files[0])
-  res.status(200).send('OK');
-});
-// to do: parsowanie pliku ze ściezki na serwerze
+port = (process.env.PORT || '3000')
+app.set('port', port);
 
-app.listen(8080, function() {
-   console.log("Listening, sir.");
-});
+var server = http.createServer(app);
+server.listen(port);
