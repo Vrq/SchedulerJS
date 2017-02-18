27c36a530faca3c1ebeb304ebf4096840222650f
Vrq <januszveiro@gmail.com>
2017-02-18 13:19:56 +0100

    minor UI changes

--- public/scripts/readCSV.js
+++ public/scripts/readCSV.js
@@ -25,6 +25,8 @@ function handleFiles(files) {
       localStorage.setItem('dataSet', JSON.stringify(response));
       $("#exampleDataSetButton").css('background-color', '#534343');
       $("#exampleDataSetButton").text("Use example dataset");
+      var hiddenDiv = $("#hiddenDiv");
+      hiddenDiv.fadeOut("fast");
       console.log('upload successful!');
     }
   });
@@ -60,7 +62,10 @@ $(document).ready(function() {
         document.getElementById("progress").value = 0;
         $("#exampleDataSetButton").css('background-color', '#2a9d8f');
         $("#exampleDataSetButton").hide().fadeIn(200).text("Example data loaded");
+        var hiddenDiv = $("#hiddenDiv");
+        hiddenDiv.fadeOut("fast");
         console.log('example dataset loaded');
+
       }
     });
   });
